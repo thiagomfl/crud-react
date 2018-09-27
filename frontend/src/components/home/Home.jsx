f7797cf85e2cf37bf508c9830d148f95150ca4d5
Thiago Moura <thmoura14@hotmail.com>
2018-09-27 05:39:51 -0300

    Projeto de Cadastro de Usuarios em React

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Cadastro de Usuarios" >
+        <div className="display-4">Bem Vindo!</div>
+        <hr/> 
+        <p className="mb-0"> Sistema de cadastro de Usuarios em React.JS </p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido por <strong> <a href="//github.com/thienry">Thiago Moura</a> </strong>
+        </span>
+    </footer>
